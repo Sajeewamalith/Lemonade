744da8c2349416b3a32ee1488eddb25c13a958f7
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-01 21:18:13 +0530

    add image background & polish UI

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,6 +20,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,12 +54,28 @@ fun LemonApp(modifier: Modifier = Modifier){
     Column(modifier = Modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
-        Image(
-            painter = painterResource(id = R.drawable.lemon_tree),
-            contentDescription = null,
-            modifier = Modifier.padding(16.dp)
-        )
+        Box (
+            modifier = Modifier
+                .size(280.dp)
+                .padding(16.dp)
+                .background(
+                    color = colorResource(R.color.background),
+                    shape = RoundedCornerShape(40.dp)
+                )
+
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.lemon_tree),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize().padding(16.dp)
+
+
+
+            )
+        }
+
         Text(
+            modifier = Modifier.padding(top = 8.dp),
             text = stringResource(R.string.lemon_tree_description),
             fontSize = 20.sp
         )
