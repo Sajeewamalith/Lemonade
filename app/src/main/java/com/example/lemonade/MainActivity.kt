28750d71288ac041980ef4f2abdc03ae275d9678
Sajeewa Malith <sajeewamalith0219@gmail.com>
2025-01-02 06:26:54 +0530

    Clickable Image
    - images and text change when click image & background

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -39,9 +44,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     LemonApp()
                 }
-
-
-
             }
         }
     }
@@ -49,6 +51,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonApp(modifier: Modifier = Modifier){
+
+    var currentIndex by remember { mutableStateOf(1) }
+
+    val imageResource = when (currentIndex) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_squeeze
+        4 -> R.drawable.lemon_squeeze
+        5 -> R.drawable.lemon_drink
+        6 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_tree
+    }
+
+    val textResource = when (currentIndex) {
+        1 -> R.string.lemon_tree_description
+        2 -> R.string.squeeze_description
+        3 -> R.string.squeeze_description
+        4 -> R.string.squeeze_description
+        5 ->  R.string.glass_of_lemonade_description
+        6 ->  R.string.empty_glass_description
+        else -> R.string.lemon_tree_description
+    }
+
     Column(modifier = Modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
@@ -60,28 +85,26 @@ fun LemonApp(modifier: Modifier = Modifier){
                     color = colorResource(R.color.background),
                     shape = RoundedCornerShape(40.dp)
                 )
-
+                .clickable{
+                    currentIndex = (currentIndex + 1)
+                    if(currentIndex > 6){
+                        currentIndex = 1
+                    }//% images.size
+                }
         ){
             Image(
-                painter = painterResource(id = R.drawable.lemon_tree),
-                contentDescription = null,
+                painter = painterResource(imageResource as Int),
+                contentDescription = currentIndex.toString(),
                 modifier = Modifier.fillMaxSize().padding(16.dp)
-
-
-
             )
         }
-
         Text(
             modifier = Modifier.padding(top = 8.dp),
-            text = stringResource(R.string.lemon_tree_description),
+            text = stringResource(textResource as Int),
             fontSize = 20.sp
         )
-
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
